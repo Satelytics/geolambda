be0a372547129bcbe0b4e5dd6e0f0f15888d5613
John Zhou <drzhouq@gmail.com>
2021-10-25 19:22:12 -0400

    added rio-tiler and plugins

--- build-and-test.sh
+++ build-and-test.sh
@@ -3,12 +3,12 @@
 VERSION=$(cat VERSION)
 PYVERSION=$(cat python/PYVERSION)
 
-docker build . -t developmentseed/geolambda:${VERSION}
-docker run --rm -v $PWD:/home/geolambda -it developmentseed/geolambda:${VERSION} package.sh
+docker build . -t satelytics/geolambda:${VERSION}
+docker run --rm -v $PWD:/home/geolambda -it satelytics/geolambda:${VERSION} package.sh
 
 cd python
-docker build . --build-arg VERSION=${VERSION} -t developmentseed/geolambda:${VERSION}-python
-docker run -v ${PWD}:/home/geolambda -t developmentseed/geolambda:${VERSION}-python package-python.sh
+docker build . --build-arg VERSION=${VERSION}   -t satelytics/geolambda:${VERSION}-python
+docker run -v ${PWD}:/home/geolambda -t satelytics/geolambda:${VERSION}-python package-python.sh
 
-docker run -e GDAL_DATA=/opt/share/gdal -e PROJ_LIB=/opt/share/proj \
-    --rm -v ${PWD}/lambda:/var/task lambci/lambda:python3.7 lambda_function.lambda_handler '{}'
+#docker run -e GDAL_DATA=/opt/share/gdal -e PROJ_LIB=/opt/share/proj \
+#    --rm -v ${PWD}/lambda:/var/task lambci/lambda:python3.7 lambda_function.lambda_handler '{}'
--- python/bin/package-python.sh
+++ python/bin/package-python.sh
@@ -33,15 +33,20 @@ for E in ${EXCLUDE}
 do
     EXCLUDES+=("--exclude ${E} ")
 done
-
-rsync -ax $PYPATH/ $DEPLOY_DIR/ ${EXCLUDES[@]}
+rsync -ax $PYPATH/ $DEPLOY_DIR ${EXCLUDES[@]}
 
 # prepare dir for lambda layer deployment - https://docs.aws.amazon.com/lambda/latest/dg/configuration-layers.html#configuration-layers-path
-cp -r $DEPLOY_DIR ./python
-rm ./python/lambda_function.py
+mkdir -p ./final_deploy/python ./final_deploy/lib ./final_deploy/share
+cp -P ${PREFIX}/lib/*.so* ./final_deploy/lib/
+cp -P ${PREFIX}/lib64/libjpeg*.so* ./final_deploy/lib/
+rsync -ax $PREFIX/share/gdal ./final_deploy/share/
+rsync -ax $PREFIX/share/proj ./final_deploy/share/
+rsync -ax $PYPATH/ ./final_deploy/python ${EXCLUDES[@]}
 
 # zip up deploy package
-zip --symlinks -ruq lambda-deploy.zip ./python
+cd ./final_deploy
+zip --symlinks -ruq ../lambda-deploy.zip  *
 
 # cleanup
-rm -rf ./python
+cd ..
+rm -rf ./final_deploy
