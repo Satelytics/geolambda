d95ce39b61939d17a40a82eb34c8bfa97a6a3218
joseph blankenship <josephblankenship@M64.bws.local>
2024-05-14 13:56:55 -0400

    updates for python 3.9 layer building with slimmed package requirements, there might be some issues include the gdal version specified

--- build-and-test.sh
+++ build-and-test.sh
@@ -9,10 +9,14 @@ docker run --rm -v $PWD:/home/geolambda -it satelytics/geolambda:${VERSION} pack
 cd python
 rm -rf lambda-deploy.zip
 sudo rm -rf lambda/*
-docker build . --build-arg VERSION=${VERSION}  --build-arg AWS_SECRET_ACCESS_KEY=$SECRET --build-arg AWS_ACCESS_KEY_ID=$KEY -t satelytics/geolambda:${VERSION}-python
+# added --no-cache so that new versions of packages will be included in layer when running script
+docker build . --no-cache --build-arg VERSION=${VERSION}  --build-arg AWS_SECRET_ACCESS_KEY=$SECRET --build-arg AWS_ACCESS_KEY_ID=$KEY -t satelytics/geolambda:${VERSION}-python
 docker run -v ${PWD}:/home/geolambda -t satelytics/geolambda:${VERSION}-python package-python.sh
 #aws s3 cp python/lambda-deploy.zip s3://sioprocess
 #aws lambda publish-layer-version --layer-name gdal_rio_ripp --content S3Bucket=sioprocess,S3Key=lambda-deploy.zip --region us-east-1 --compatible-runtimes python3.7 python3.8 python3.9
 #aws s3 rm  s3://sioprocess/lambda-deploy.zip
 #docker run -e GDAL_DATA=/opt/share/gdal -e PROJ_LIB=/opt/share/proj \
 #    --rm -v ${PWD}/lambda:/var/task lambci/lambda:python3.7 lambda_function.lambda_handler '{}'
+
+# No need to automatically publish until we standardize this
+# aws lambda publish-layer-version --layer-name "geolambda_python_3.9.9" --description "Description of your layer" --zip-file fileb://path/to/your/layer.zip
